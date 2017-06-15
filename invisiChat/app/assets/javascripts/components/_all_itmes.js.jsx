78c2b2ab79db6c3fce9d1d53499f73c66866226a
johnconnor31 <johnconnor31@github.com>
2017-06-15 20:28:36 +0530

    Edit functionality

--- invisiChat/app/assets/javascripts/components/_all_itmes.js.jsx
+++ invisiChat/app/assets/javascripts/components/_all_itmes.js.jsx
@@ -1,12 +1,14 @@
 var AllItems= React.createClass({
   render(){
     var delHandler= this.props.deleteHandle;
+    var editHandler= this.props.editHandle;
     var items= this.props.items.map(function(item){
-      return (
-        
-      );
-    });
+      return (<Item id={item.id} name={item.name} description={item.description}
 
+                delHandler={delHandler} editHandler={editHandler}/>
+          );
+      });
+      
     return (
       <div>
         {items}
--- invisiChat/app/assets/javascripts/components/_item.js.jsx
+++ invisiChat/app/assets/javascripts/components/_item.js.jsx
@@ -1,8 +1,27 @@
 var Item = React.createClass({
+	onInitialize(){
+		this.state={
+			editMode:false,
+			name:null,
+			description:null,
+		}	
+	}
+	onEdit(){
+		if(this.state.editMode){
+
+		}
+		this.state={
+			editMode:!this.state.editMode
+		}
+	}
+
   render(){
+  	var nameField= this.state.editable? <input name='name' value={this.props.name}/>:{this.props.name};
+  	var textField= this.state.editable?<input name='description' value={this.props.description}/>:{this.props.description};
     return(<div key={this.props.id}>
-        <div>{this.props.name}</div>
-        <div>{this.props.description}<button onClick={()=>{this.props.delHandler(item.id);}}>Remove</button>
+    	<div>{nameField}</div>
+    	<div>{textField}</div>
+        <div><button onClick={()=>{this.props.delHandler(item.id);}}>Remove</button>
         <button onClick={()=>{this.props.editHandler(item.id);}}>Edit</button>
         </div>
         </div>);
